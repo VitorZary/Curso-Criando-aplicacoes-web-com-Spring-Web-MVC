611819bea5ac775fb63a01d91d7d1009cf3b12b3
Vitor Hugo Kuhn Zary <vitorhbts@gmail.com>
2021-10-08 11:05:00 -0300

    Atualizando o codigo para aula numero 2

--- src/main/java/com/spring/web/repository/JediRepository.java
+++ src/main/java/com/spring/web/repository/JediRepository.java
@@ -1,28 +1,14 @@
 package com.spring.web.repository;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import com.spring.web.model.Jedi;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class JediRepository {
+public interface JediRepository extends JpaRepository<Jedi, Long> {
 
-    private List<Jedi> jedi;
+    List<Jedi> findByNameContainingIgnoreCase(final String name);
 
-    public JediRepository() {
-        jedi = new ArrayList<>();
-        jedi.add(new Jedi("Luke", "Skywalker"));
-        jedi.add(new Jedi("Obi-Wan", "Kenobi"));
-        jedi.add(new Jedi("Qui-Gon", "Jinn"));
-    }
-
-    public List<Jedi> getAll() {
-        return jedi;
-    }
-
-    public void add(final Jedi jedi) {
-       this.jedi.add(jedi);
-    }
 }
